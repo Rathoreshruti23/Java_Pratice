eae670e1a17ffc4c56ac8110cf000c02498cdc1b
Rathoreshruti23 <shrutirathore892@gmail.com>
2025-10-15 15:13:31 +0530

    updated code

--- Itfirm/src/main/java/com/shrunity/Itfirm/ItfirmApplication.java
+++ Itfirm/src/main/java/com/shrunity/Itfirm/ItfirmApplication.java
@@ -2,13 +2,16 @@ package com.shrunity.Itfirm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static io.restassured.RestAssured.given;
+
 @SpringBootApplication
 public class ItfirmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItfirmApplication.class, args);
 		System.out.println("hello");
-
+		given().baseUri("http://localhost:8080").header("Content-Type","application/json").header("Authorization","Bearer <token>")
+				.when().get("/api/product").then().statusCode(200);
 
 
 	}
